2c0f707c6a03b9cab6a076a0d8808ded886b0d68
Pedram <Shemuel1226@gmail.com>
2022-08-10 21:39:11 +0430

    Api Project Added & A1 Method Added

---
+++ Solution1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult A1(string test)
+        {
+            return Ok("Done !");
+        }
+
+    }
+}
