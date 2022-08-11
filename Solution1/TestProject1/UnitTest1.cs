9b787f4c628d4c6551e73b2c56a9ad4844084790
Pedram <Shemuel1226@gmail.com>
2022-08-11 11:46:48 +0430

    Boolean Test Done!

--- Solution1/TestProject1/UnitTest1.cs
+++ Solution1/TestProject1/UnitTest1.cs
@@ -1,14 +1,35 @@
 using System;
+using WebApplication1.Controllers;
 using Xunit;
 
 namespace TestProject1
 {
     public class UnitTest1
     {
-        [Fact]
+        //private readonly TestController testController;
+        //public UnitTest1(TestController testController)
+        //{
+        //    this.testController = testController;   
+        //}
+        [Fact] 
         public void Test1()
         {
-
+            Assert.True(A3());
+        } 
+        [Fact] 
+        public void Test2()
+        {
+            Assert.NotNull(A3());
+            Assert.NotEqual(false,A3());
+        }   
+        [Fact] 
+        public void Test3()
+        {
+            Assert.NotNull(A3());
+        }
+        public bool A3()
+        {
+            return true;
         }
     }
 }
--- Solution1/WebApplication1/Controllers/TestController.cs
+++ Solution1/WebApplication1/Controllers/TestController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -15,6 +16,16 @@ namespace WebApplication1.Controllers
         public IActionResult A1(string test)
         {
             return Ok("Done !");
+        }     
+        [HttpPost]
+        public IActionResult A2([FromBody]Models.Person person)
+        {
+            return Ok("Done !");
+        }
+        
+        public bool A3()
+        {
+            return true;
         }
 
     }
