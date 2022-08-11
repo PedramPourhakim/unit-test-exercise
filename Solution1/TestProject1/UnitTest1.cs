43c71355bd7d0db8d7e939650ed0706d6fd462b7
Pedram <Shemuel1226@gmail.com>
2022-08-11 12:54:29 +0430

    Project Succssfully Done !

--- Solution1/TestProject1/UnitTest1.cs
+++ Solution1/TestProject1/UnitTest1.cs
@@ -19,17 +19,52 @@ namespace TestProject1
         [Fact] 
         public void Test2()
         {
-            Assert.NotNull(A3());
-            Assert.NotEqual(false,A3());
+            int id = 10;
+            Assert.NotEqual(0,A4(id));
         }   
         [Fact] 
         public void Test3()
         {
-            Assert.NotNull(A3());
+            Assert.NotNull(A5());
+        }   
+        [Theory] 
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void Test4(int id)
+        {
+            Assert.NotEqual(0,A4(id));
         }
         public bool A3()
         {
             return true;
         }
+        public int A4(int id)
+        {
+            return 1;
+        }
+        public string A5()
+        {
+            return "Hello World";
+        }
+
+        public double sum(double d1, double d2) => d1 + d2;
+
+        [Theory]
+        [InlineData(6,8)]
+        [InlineData(9,1)]
+        [InlineData(10,23)]
+        [InlineData(43,14)]
+        public void TestSum(double p1,double p2)
+        {
+            Assert.Equal((p1 + p2), sum(p1, p2));
+        }
+        public static bool A6(WebApplication1.Models.Person person)
+        {
+            return true;
+        }
+
     }
 }
---
+++ Solution1/TestProject1/UnitTest2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProject1
+{
+    public class UnitTest2
+    {
+        [Theory]
+        [MemberData(nameof(Users))]
+        public void Add(WebApplication1.Models.
+            Person person)
+        {
+            Assert.True(UnitTest1.A6(person));
+        }
+        public static TheoryData<WebApplication1.Models.
+            Person> Users()
+        {
+                var data = new TheoryData<WebApplication1.Models.
+            Person>(); 
+                WebApplication1.Models.
+            Person person1 = new WebApplication1.Models.Person()
+            {
+                FirstName = "Pedram",
+                LastName = "Pourhakim",
+                Age = 23
+            };
+                data.Add(person1);
+                return data;
+            }
+        }
+
+          
+    }
